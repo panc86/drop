28126464437ce1ffd96e290bce2c4f3037f38e61
Emanuele Panizio <panc86@gmail.com>
2024-09-26 11:03:11 +0200

    update kafka consumer group id and topic id, increase batch size to 256 and remove comments

--- transformers/text_annotator/src/consumer.py
+++ transformers/text_annotator/src/consumer.py
@@ -13,36 +13,31 @@ default_config = {
     # User-specific properties that you must set
     "bootstrap.servers": "broker:29092" if IS_DOCKER_ENV else "localhost:9092",
     # Fixed properties
-    "group.id": "data_point_group",
+    "group.id": "annotation",
     "auto.offset.reset": "earliest",
 }
 
 
 def consume_events(topics: list[str], handler, config: dict = default_config):
-    outgoing_topic = "annotated_texts"
     consumer = Consumer(config)
-    consumer.subscribe(topics)
     # model prediction is computationally expensive and should be executed in batches
     batch = []
     try:
+        consumer.subscribe(topics)
         while True:
             msg = consumer.poll(1.0)
             if msg is None:
-                # Initial message consumption may take up to
-                # `session.timeout.ms` for the consumer group to
-                # rebalance and start consuming
                 if len(batch) > 0:
-                    print("executes batch leftover...")
-                    publish_events(handler(batch), outgoing_topic)
+                    publish_events(handler(batch), "annotated_texts")
+                    print(f"processed {len(batch)} events...")
                     batch = []
-                print("Waiting...")
             elif msg.error():
                 print("ERROR: {}".format(msg.error()))
             else:
-                # print(dict(topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
                 batch.append(json.loads(msg.value().decode("utf-8")))
-                if len(batch) == 32:
-                    publish_events(handler(batch), outgoing_topic)
+                if len(batch) == 256:
+                    publish_events(handler(batch), "annotated_texts")
+                    print(f"processed {len(batch)} events...")
                     batch = []
     except KeyboardInterrupt:
         print("\nInterrupted")
