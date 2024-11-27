8e888fc594241a74e7a176bcfcbf0ab880a0dc16
Emanuele Panizio <panc86@gmail.com>
2024-11-27 21:26:15 +0100

    fix extractors tests

--- pipeline/extractors/src/test/test_sources/test_emm.py
+++ pipeline/extractors/src/test/test_sources/test_emm.py
@@ -1,11 +1,9 @@
-from datetime import datetime
-
 from app.sources.emm import build_request_url
 
 
 def test_build_request_url():
     extraction = dict(
-        begin=datetime(2024, 9, 5), end=datetime(2024, 9, 6), keywords="test,keyword"
+        begin="2024-09-05", end="2024-09-06", keywords="test,keyword"
     )
     expected = "https://emm.newsbrief.eu/rss/rss?language=all&type=search&mode=advanced&datefrom=2024-09-05T00%3A00%3A00Z&dateto=2024-09-06T00%3A00%3A00Z&atLeast=test%2Bkeyword&duplicates=false"
     assert build_request_url(extraction) == expected
